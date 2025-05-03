b4bf75bf47124c38bdccc101b8717a7f1a6b8f79
Ukasyah <hi.ukasyah@gmail.com>
2025-05-03 17:13:01 +0700

    add axum hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route("/", get(|| async { "Hello from helix-api!" }));
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    println!("Listening on http://0.0.0.0:3000");
+
+    axum::serve(listener, router).await.unwrap();
 }
