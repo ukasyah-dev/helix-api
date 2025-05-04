31204affbdac493dcb569c7be569d045de8d640e
Ukasyah <hi.ukasyah@gmail.com>
2025-05-04 17:57:14 +0700

    add util

---
+++ src/util/validator.rs
@@ -0,0 +1,17 @@
+use validator::Validate;
+
+use super::error;
+
+pub fn validate<T: Validate>(value: &T) -> Result<(), error::Error> {
+    if let Err(err) = value.validate() {
+        if let Some((field, errors)) = err.field_errors().iter().next() {
+            let message = errors.first().map(|e| e.to_string()).unwrap_or_default();
+
+            return Err(error::invalid_argument_with_message(&format!(
+                "{}: {}",
+                field, message
+            )));
+        }
+    }
+    Ok(())
+}
